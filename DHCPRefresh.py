1b72fca2f64a1603ba1c9f581aa0b53c8eb4994e
CrimsonTorso <40295915+CrimsonTorso@users.noreply.github.com>
2019-01-24 14:22:41 +0000

    Update DHCPRefresh.py

--- DHCPRefresh.py
+++ DHCPRefresh.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
-import os
-import time
+import os #Imports the os module
+import time #Imports the time module
 
 def clear():
-  responce = os.system("clear")
+  responce = os.system("clear") #Clears the CLI (Command Line Interface)
   
 def ref
-  time.sleep(2.5)
-  
+  responce = os.system("ipconfig /release") #Releases the current Dynamic DHCP IP
+  time.sleep(0.5) #Sleeps for 0.5 seconds
+  responce = os.system("ipconfig /renew") #Renews a new IP through a IP list connected with your ISP
+  time.sleep(0.5) #Sleeps for 0.5 seconds
+ 
+clear() #Calls the 'clear' function
+ref() #Calls the 'ref' function
+responce = os.system("./DHCPRefresh.py") #Re-opens the program again
+
+# Written By CrimsonTorso
